aa1127c54a3a9d68729f541156d379c41bd0297c
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-10-17 18:31:24 +0530

    blank main.c

--- 02/main.c
+++ 02/main.c
@@ -3,7 +3,7 @@
  * Group 02 :
  * 210020009 - Ganesh Panduranga Karamsetty
  * 210020036 - Rishabh Pomaje
- * Program to setup the I2C module on the muC and interface an OLED device via I2C.
+ * Program to setup the I2C module on the muC and communicate with itself or other muC.
 */
 #define CLOCK_HZ    16000000
 
@@ -11,26 +11,9 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
-void CLK_enable( void );
-void PORT_E_init( void );
-
-int main(void)
+int main( void )
 {
-
-}
-
-void CLK_enable( void )
-{
-
-}
-
-void PORT_E_init( void )
-{
-    GPIO_PORTE_LOCK_R = 0x4C4F434B ;                            // Unlock commit register
-    GPIO_PORTE_CR_R = 0xF1 ;                                    // Make PORT-E configurable
-    GPIO_PORTE_DEN_R = 0x03 ;                                   // Set PORT-E pins as digital pins
-    GPIO_PORTE_DIR_R = 0x02 ;                                   // Set PORT-E pin directions
-    GPIO_PORTE_PUR_R = 0x02 ;                                   // Pull-Up-Resistor Register
-    GPIO_PORTE_AFSEL_R = 0x03 ;                                 // Alternate function select for the PE0 and PE1
-    GPIO_PORTE_PCTL_R = 0x11 ;                                  // Selecting the peripheral for the driving AFSEL
+    while(1){
+        ;
+    }
 }
