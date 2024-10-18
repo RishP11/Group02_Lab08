1be94895801b58f399375df95ba676b2a1b0e669
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-10-18 10:31:53 +0530

    added port_e_init fn

--- 02/main.c
+++ 02/main.c
@@ -25,6 +25,18 @@ int main( void )
     }
 }
 
+void PORT_E_init( void )
+{
+    GPIO_PORTE_LOCK_R = 0x4C4F434B ;                            // Unlock commit register
+    GPIO_PORTE_CR_R = 0xF1 ;                                    // Make PORT-E configurable
+    GPIO_PORTE_DEN_R = (1 << 5) | (1 << 4) ;                                   // Set PORT-E pins as digital pins
+//    GPIO_PORTE_DIR_R = (1 << 5) | (1 << 4) ;                                   // Set PORT-E pin directions
+    GPIO_PORTE_PUR_R = (1 << 5) | (1 << 4) ;                                   // Pull-Up-Resistor Register
+    GPIO_PORTE_AFSEL_R = (1 << 5) | (1 << 4) ;                                 // Alternate function select for the PE0 and PE1
+    GPIO_PORTE_PCTL_R = 0x330000 ;                                  // Selecting the peripheral for the driving AFSEL
+    GPIO_PORTB_ODR_R |= (1 << 5) ;                                        // Set SDA line in Open Drain Configuration
+}
+
 void PORT_F_init( void )
 {
     GPIO_PORTF_LOCK_R = 0x4C4F434B ;                            // Unlock commit register
